b4c0d22dfe4b1ebb2be7f40042841ab61489ce3f
Le Duy Thuc <leduythuc.cs@gmail.com>
2021-03-08 23:32:56 +0700

    Move feedback to extended_feedback

--- dmoj/checkers/bridged.py
+++ dmoj/checkers/bridged.py
@@ -67,7 +67,8 @@ def check(
             point_value,
             time_limit,
             memory_limit,
-            feedback=utf8text(proc_output) if feedback else '',
+            feedback='',
+            extended_feedback=utf8text(proc_output) if feedback else '',
             name='checker',
             stderr=error,
         )
--- dmoj/tests/test_checker.py
+++ dmoj/tests/test_checker.py
@@ -1,5 +1,13 @@
 import unittest
 
+from dmoj.result import CheckerResult
+
+
+def check_to_bool(result):
+    if isinstance(result, CheckerResult):
+        return result.passed
+    return bool(result)
+
 
 class CheckerTest(unittest.TestCase):
     def assert_pass(self, check, str1, str2, expect=True):
@@ -7,13 +15,34 @@ class CheckerTest(unittest.TestCase):
             message = 'expecting %r to equal %r'
         else:
             message = 'expecting %r to not equal %r'
-        self.assertEqual(bool(check(str1, str2)), expect, message % (str1, str2))
-        self.assertEqual(bool(check(str2, str1)), expect, message % (str2, str1))
+        self.assertEqual(check_to_bool(check(str1, str2, point_value=1.0)), expect, message % (str1, str2))
+        self.assertEqual(check_to_bool(check(str2, str1, point_value=1.0)), expect, message % (str2, str1))
 
     def assert_fail(self, check, str1, str2):
         self.assert_pass(check, str1, str2, expect=False)
 
     def test_standard(self):
+        from dmoj.checkers.standard import check
+
+        self.assert_pass(check, b'a', b'a')
+        self.assert_pass(check, b'a b', b'a  b')
+        self.assert_pass(check, b'a b   \n', b'a b')
+        self.assert_pass(check, b'\n\na b \n    ', b'a b')
+        self.assert_pass(check, b'a\n\n\nb', b'a\nb')
+        self.assert_pass(check, b'  a   \n\n', b'\n\n\n  a   \n')
+        self.assert_pass(check, b'a ' * 1000, b' a' * 1000)
+        self.assert_pass(check, b'a\n' * 1000, b'\n\n\na' * 1000)
+
+        self.assert_fail(check, b'a', b'b')
+        self.assert_fail(check, b'\n\n\na \n b\n', b'a b')
+        self.assert_fail(check, b'a\n\n\nb', b'a b')
+        self.assert_fail(check, b'ab', b'a b')
+
+        # Checkers should handle mixed bytes/str
+        self.assert_pass(check, b'a', u'a')
+        self.assert_fail(check, b'a', u'b')
+
+    def test_linecount(self):
         from dmoj.checkers.standard import check
 
         self.assert_pass(check, b'a', b'a')
@@ -40,11 +69,11 @@ class CheckerTest(unittest.TestCase):
         def is_pe(res, feedback='Presentation Error, check your whitespace'):
             return res is not True and not res.passed and res.feedback == feedback
 
-        assert check(b'a\nb\nc', b'a\nb\nc')
-        assert check(b'a\nb\nc', b'a\nb\nc')
-        assert is_pe(check(b'a \nb\nc', b'a\nb\nc'))
-        assert is_pe(check(b'a\nb\nc', b'a\nb\nc\n'))
-        assert is_pe(check(b'a\nb\nc', b'a\nb\nc\n', pe_allowed=False), feedback=None)
+        assert check(b'a\nb\nc', b'a\nb\nc', point_value=1.0)
+        assert check(b'a\nb\nc', b'a\nb\nc', point_value=1.0)
+        assert is_pe(check(b'a \nb\nc', b'a\nb\nc', point_value=1.0))
+        assert is_pe(check(b'a\nb\nc', b'a\nb\nc\n', point_value=1.0))
+        assert is_pe(check(b'a\nb\nc', b'a\nb\nc\n', pe_allowed=False, point_value=1.0), feedback=None)
 
     def test_sorted(self):
         from dmoj.checkers.sorted import check
